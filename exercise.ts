11035a8aa480b61eb04008ca13cdc510d0a02ba3
Nahalil <nahalilmuchtar2@gmail.com>
2025-01-27 09:53:26 +0800

    selesai semua exercise 3

--- exercise.ts
+++ exercise.ts
@@ -1,5 +1,5 @@
 // soal 1
-let n1: number = 9;
+const n1: number = 9;
 let multi: number = 0;
 
 for (let i = 1; i <= n1 ; i++) {
@@ -71,7 +71,7 @@ for (let i = 0; i < chars.length; i++) {
 console.log(charsLeft);
 
 //soal 6
-let w3: string = "hello world";
+const w3: string = "hello world";
 
 // split words and store it in an array
 const words = w3.split(" ");
@@ -86,3 +86,89 @@ words.forEach(e => {
 });
 
 console.log(result.trim());
+
+// soal 7
+const w4: string = "The QuiCK BrOwN Fox";
+
+let chars2 = [];
+
+for (let i = 0; i < w4.length; i++) {
+    if (w4[i] == w4[i].toUpperCase()) {
+        let lower: string = w4[i].toLowerCase();
+        chars2.push(lower);
+    } else if (w4[i] == w4[i].toLowerCase()) {
+        let upper: string = w4[i].toUpperCase();
+        chars2.push(upper);
+    }
+}
+
+// a variable to store chars left
+let result1: string = ""; 
+
+// turn left chars in array into a string
+for (let i = 0; i < chars2.length; i++) {
+    result1 += chars2[i];
+}
+
+console.log(result1);
+
+// soal 7
+let num1: number = 42;
+let num2: number = 54;
+
+console.log(num1 > num2 ? num1 : num2)
+
+// soal 8
+let number1: number = 42;
+let number2: number = 27;
+let number3: number = 18;
+
+if (number1 < number2 && number1 < number3) {
+    if (number2 < number3) {
+        console.log(`${number1}, ${number2}, ${number3}`);
+    } else {
+        console.log(`${number1}, ${number3}, ${number2}`);
+    };
+} else if (number2 < number1 && number2 < number3) {
+    if (number1 < number3) {
+        console.log(`${number2}, ${number1}, ${number3}`);
+    } else {
+        console.log(`${number2}, ${number3}, ${number1}`);
+    };
+} else if (number3 < number1 && number3 < number2) {
+    if (number2 < number1) {
+        console.log(`${number3}, ${number2}, ${number1}`);
+    } else {
+        console.log(`${number3}, ${number1}, ${number2}`);
+    };
+};
+
+// soal 9
+const input = "Hello";
+
+if (typeof input == "string") {
+    console.log(1);
+} else if (typeof input == "number") {
+    console.log(2);
+} else {
+    console.log(3);
+}
+
+// soal 10
+const sentence: string = "An apple a day keep the doctor away";
+
+let change = [];
+let result2 = "";
+
+for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] == "a" || sentence[i] == "A") {
+        change.push("*");
+    } else {
+        change.push(sentence[i]);
+    }
+}
+
+for (let i = 0; i < change.length; i++) {
+    result2 += change[i];    
+}
+console.log(result2);
